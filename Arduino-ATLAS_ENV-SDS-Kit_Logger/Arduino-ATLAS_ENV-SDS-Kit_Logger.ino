4e0a0006f80aa885b1087b201aceca7eba225392
jknappe <jan.knappe@tcd.ie>
2016-10-11 18:05:28 +0100

    initialize SD card

--- Arduino-ATLAS_ENV-SDS-Kit_Logger/Arduino-ATLAS_ENV-SDS-Kit_Logger.ino
+++ Arduino-ATLAS_ENV-SDS-Kit_Logger/Arduino-ATLAS_ENV-SDS-Kit_Logger.ino
@@ -13,9 +13,21 @@
 // SETUP
 //-------------------------------------------------------------------------------------------
 
-#include <Wire.h>                   // connecting to RTC
-#include "RTClib.h"                 // using RTC for timestamp
-  RTC_DS1307 RTC;                   // define RTC object
+// INCLUDE LIBRARIES
+//---------------------------------------  
+#include <Wire.h>                   //Connecting to RTC
+#include "RTClib.h"                 //Using RTC for timestamp
+  RTC_DS1307 RTC;                   //Define RTC object
+#include <SD.h>                     //Using SD card to log data 
+  File SDdata;                      //Define SDdata object 
+
+// DEFINE PINS
+//---------------------------------------  
+const byte SDPin = 10;              //SD shield CS pin (10 on datalogging shield, 53 on MEGA)
+
+// DEFINE VARIABLES
+//---------------------------------------  
+
   
 //==========================================================================================
 
@@ -31,19 +43,34 @@ void setup() {
 
 // SETTING RTC
 //---------------------------------------  
+  Serial.print("Initializing RTC: ");
+  
   if (!RTC.begin()) {                                       //IF RTC is not found
     Serial.println("RTC not found.");                       //Print error message
     while(1);                                               //And halt program
   }                                                         //End IF    
-
-  if (!RTC.isrunning()) {                                     //IF RTC is not running
+  Serial.println("successful"); 
+  
+  if (!RTC.isrunning()) {                                   //IF RTC is not running
     RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));         //Initialize RTC with time of compilation
   }                                                         //End IF    
   
   Serial.print("RTC time is set to: ");
   printNowTime();                                           //Print current RTC time
   Serial.println();
-  Serial.println("Recompile sketch to reset RTC to system time.");
+  Serial.println("Change battery and compile sketch to reset RTC to system time.");
+
+// SETTING SD CARD
+//---------------------------------------    
+  pinMode(SDPin, OUTPUT);                                   //Set SDPin as output
+    digitalWrite(SDPin, HIGH);                              //Activate internal pullup resistor
+  Serial.print("Initializing SD Card: ");
+  
+  if (!SD.begin(SDPin)) {                                   //IF SD is not found
+    Serial.println("SD not found.");                        //Print error message
+    while(1);                                               //And halt program
+  }                                                         //End IF    
+  Serial.println("successful.");
   
 }
 //==========================================================================================
